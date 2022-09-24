94c96402cb88f094b3bdc4edc3208a14a98c96c8
NASINE DE PREZ Eduardo Ezequiel <e2ndp@gmail.com>
2022-09-24 20:47:29 -0300

    Funciones Flecha y Anonimas Agregadas

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 /*  Simulador --> Control de Temperatura Hogareño 
     ---------------------------------------------
-    Funciona en dos franjas horarias:   06:00hs - 08:00hs   (al levantarse, corta cuando se sale al Trabajo, Colegio, etc.)
-                                        16:30hs - 22:30hs   (al regresar del Trabajo, Colegio, etc.; hasta la hora de acostarse)
+    Funciona en dos franjas horarias:   06:00hs - 07:00hs   (Habitación: al levantarse, corta cuando se pasa al Living)
+                                        16:30hs - 22:30hs   (Living: cuando se sale de la Habitación, corta al salir de casa.)
         Por una cuestión de simplificación, la hora se toma como Float (ej. 07:30hs --> 7.5 [hs])
     
     Parámetros que recibe:  Temp. Actual
-                            Temp. Deseada
-                            Hora Actual                     
+                            Hora Actual
+                            
+                            (*) Temp. Deseada, lo saca del Array de Tempraturas Guardadas por Ambientes
                             
     Los ingresos son por medio de funciones que controlan los datos ingresados      */
 
 
 
-/*  Clases  */
+/*  ----------
+    | Clases |
+    ----------  */
 class tempProgramada {
     constructor(ambiente, tDeseada, hInicio, hFinal){
         this.ambiente = ambiente;
@@ -26,38 +29,19 @@ class tempProgramada {
     }
 }
 
-/*  Array de Temperaturas   */
+/*  Array de Temperaturas diferentes Ambientes  */
 tempAmbientes = [];
-
 /*  Instancias (Objetos) de la Clase 'tempProgramada'    */
 tempAmbientes.push(new tempProgramada('Habitacion', 28 , 6.00 , 7.00));
-
 tempAmbientes.push(new tempProgramada('Living', 25 , 7.00 , 8.00));
 
+/*  Array que almacena el Consumo Energetico por Ambiente */
+consAmbiente = [];
 
-
-
-
-
-
-/*  Funciones   */
-function controlTemperatura(temperaturaActual, horaActual) {
-    for(const amb of tempAmbientes){
-        if(((horaActual >= amb.hInicio) && (horaActual <= amb.hFinal))) {
-            if(temperaturaActual < amb.tDeseada){
-                alert('Calentando el ambiente: ' + amb.ambiente);
-            }else if(temperaturaActual > amb.tDeseada){
-                alert('Enfriando el ambiente: ' + amb.ambiente);
-            }else{
-                alert(amb.ambiente + ' con la Temperatura en su punto justo...');
-            }
-            alert('Temperatura Controlada en Ambiente: ' + amb.ambiente);
-        } else {
-            alert('Horario Fuera de Control para el Ambiente: ' + amb.ambiente);
-        }
-    }
-}
-
+/*  -------------
+    | Funciones |
+    -------------   */
+/*  Funcion que regula el ingreso de la Temperatura que sea algo acorde */
 function ingresoTemperatura() {
     let temperatura = 0;
     
@@ -68,6 +52,7 @@ function ingresoTemperatura() {
     return temperatura;
 }
 
+/*  Funcion que regula el ingreso de una hora acorde    */
 function ingresoHora() {
     let hora = 0;
 
@@ -78,7 +63,36 @@ function ingresoHora() {
     return hora;
 }
 
-/*  Ejecución Principal */
+/*  Función Flecha --> calcula la diferencia de Temperaturas */
+const dTemp = (may, men) => may - men; 
+
+/* Funcion que controla la Temperatura Ambiente pasandoles como parámetros la hora y la temperatura*/
+function controlTemperatura(temperaturaActual, horaActual) {
+    for(const amb of tempAmbientes){
+        if(((horaActual >= amb.hInicio) && (horaActual <= amb.hFinal))) {
+            if(temperaturaActual < amb.tDeseada){
+                alert('Calentando el ambiente: ' + amb.ambiente);
+                consAmbiente.push( dTemp(amb.tDeseada, temperaturaActual) );
+            }else if(temperaturaActual > amb.tDeseada){
+                alert('Enfriando el ambiente: ' + amb.ambiente);
+                consAmbiente.push( dTemp(temperaturaActual, amb.tDeseada) );
+            }else{
+                alert(amb.ambiente + ' con la Temperatura en su punto justo...');
+            }
+            alert('Temperatura Controlada en Ambiente: ' + amb.ambiente);
+        } else {
+            alert('Horario Fuera de Control para el Ambiente: ' + amb.ambiente);
+        }
+    }
+}
+
+/*  Funcion Anónima --> calcula el consumo total suponiendo que por cada °C se consume 1.27kW */
+const consumoTotal = function(dt) {return dt * 1.27};
+
+
+/*  -----------------------
+    | Ejecución Principal |
+    -----------------------  */
 let cantidadMediciones = parseInt(prompt('Ingrese cuantas mediciones desea realizar: '));
 
 for(let i=0; i < cantidadMediciones; i++){
@@ -89,5 +103,9 @@ for(let i=0; i < cantidadMediciones; i++){
 
     controlTemperatura(tempActual, hoActual);
 }
+
+/*  Calculo el Consumo Total    */
+const consumoParcial = consAmbiente.reduce( (acc, consumo) => acc + consumo, 0);
+alert('Se consumieron ' + consumoTotal(consumoParcial) + ' kW en el proceso.');
 
 alert('Gracias por usar nuestro Sistema ;-)');
